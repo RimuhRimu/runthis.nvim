b93af9b9ced3e0587aecde75cd7c314294af222c
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2023-01-05 23:53:09 -0400

    testing precommit

--- lua/attach.lua
+++ lua/attach.lua
@@ -1,58 +1,93 @@
 local v = vim.api
-local _postWriteRef = {}
 
-local attach_to_buf = function(command,opts,name,path)
-  v.nvim_create_autocmd({"BufWritePost"}, {
-      group = v.nvim_create_augroup("AutoRunNode", {clear = true}),
-      callback = function ()
-          if not _postWriteRef["buf"] then
-              _postWriteRef["buf"] = v.nvim_create_buf(true,true)
-              v.nvim_buf_set_option(_postWriteRef["buf"],"filetype","javascript")
-          end
+-- local state to save references
+local _postWriteRef = {
+	-- {buf[n]} = {
+	--   win = number,
+	--   client = number
+	-- },
+}
 
-          local bufnr, winnr = _postWriteRef["buf"],_postWriteRef["win"]
+local function bufExists(name)
+	for _, buf in pairs(_postWriteRef) do
+		if buf.client == name then
+			return true
+		end
+	end
+end
 
-          local winExists = false
+local attach_to_buf = function(command, client, clientBuf)
+	local name, path = client.name, "/" .. client.path
 
-          for _, val in pairs(v.nvim_list_wins()) do
-              if val == winnr then
-                  winExists = true
-                  break
-              end
-          end
+	local auGroup = v.nvim_create_augroup("AutoRun " .. name, { clear = true })
+	v.nvim_create_autocmd({ "BufWritePost" }, {
+		desc = "Creates a buffer to show what's executed on save for the given buf as client",
+		group = auGroup,
+		callback = function()
+			local pluginBufnr
 
-          if not winExists then
-              vim.cmd[[vsplit]]
-              winnr = v.nvim_get_current_win()
-              v.nvim_win_set_buf(winnr,bufnr)
-              winExists = true
-          end
+			if not bufExists(name) then
+				local newBuf = v.nvim_create_buf(true, true)
+				_postWriteRef[newBuf] = {}
+				pluginBufnr = newBuf
+				local clientFT = v.nvim_buf_get_option(clientBuf, "filetype")
+				v.nvim_buf_set_option(pluginBufnr, "filetype", clientFT)
+			else
+				for buffer, content in pairs(_postWriteRef) do
+					if name == content.client then
+						pluginBufnr = buffer
+					end
+				end
+			end
 
-          local handleStd = function(_,data)
-              if data and table.concat(data) ~= "" then
-                  table.insert(data,1,("Running %s %s on %s"):format(command,opts,name))
-                  table.insert(data,2,"-")
-                  table.insert(data,2," ")
-                  v.nvim_buf_set_lines(bufnr,0,-1,false,data)
-              end
-          end
+			local winnr = _postWriteRef[pluginBufnr].win
 
-          local fullCommand
-          if opts == "" then
-            fullCommand = command
-          else
-            fullCommand = command.." "..opts
-          end
+			local winExists = false
 
-          vim.fn.jobstart({fullCommand,path},{
-              stdout_buffered = true,
-              on_stdout = handleStd,
-              on_stderr = handleStd
-          })
+			for _, val in pairs(v.nvim_list_wins()) do
+				if val == winnr then
+					winExists = true
+					break
+				end
+			end
 
-          _postWriteRef["win"] = winnr
-      end,
-      pattern = name
-  })
+			if not winExists then
+				vim.cmd([[vsplit]])
+				winnr = v.nvim_get_current_win()
+				v.nvim_win_set_buf(winnr, pluginBufnr)
+				v.nvim_win_set_option(winnr, "winfixwidth", true)
+				v.nvim_win_set_width(winnr, 40)
+				winExists = true
+			end
+
+			local handleStdout = function(_, data)
+				if data and table.concat(data) ~= "" then
+					table.insert(data, 1, ("Running %s on '%s'"):format(command, name))
+					table.insert(data, 2, "-")
+					table.insert(data, 2, " ")
+					v.nvim_buf_set_lines(pluginBufnr, 0, -1, false, data)
+				end
+			end
+
+			local finalCommand
+			if command == "" then
+				finalCommand = { path }
+			else
+				finalCommand = { command, path }
+			end
+			vim.fn.jobstart(finalCommand, {
+				stdout_buffered = true,
+				on_stdout = handleStdout,
+				on_stderr = handleStdout,
+			})
+
+			-- Save the state for this buf
+			_postWriteRef[pluginBufnr] = {
+				win = winnr,
+				client = name,
+			}
+		end,
+		pattern = name,
+	})
 end
 return attach_to_buf
