824e75a8675f6dc54cd771324069b8731c175a82
RimuhRimu <rimuhrimu@protonmail.com>
2023-06-28 22:03:20 -0400

    feat(prompt): imporved prompt options

--- plugin/runthis.lua
+++ plugin/runthis.lua
@@ -1,16 +1,20 @@
 local v = vim.api
 local runthis = require("runthis")
-local completionSource = {
+local completionOptions = {
 	[""] = { "attach", "detach" },
+	["first"] = { "attach", "detach" },
 	primary = {},
 }
-local function completion(_, CmdLine, _)
+local function completion(ArgLead, CmdLine, CursorPos)
 	-- local hasSpace = string.match(CmdLine, "%s$")
 	local params = vim.split(CmdLine, "%s+", { trimempty = true })
-	if #params == 1 then
-		return completionSource[""]
+	P(params)
+	if params[2] == "attach" or params[2] == "detach" then
+		return completionOptions["first"]
+	elseif params[1] == "RunThis" and #params <= 2 then
+		return completionOptions[""]
 	end
-	return completionSource
+	return completionOptions
 end
 
 v.nvim_create_user_command("RunThis", runthis.prompt, { nargs = "?", complete = completion })
